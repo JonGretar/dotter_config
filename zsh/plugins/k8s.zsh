a88190bdff136605e86d1b4ce51817c5d4217753
Jón Grétar Borgþórsson <jongretar@jongretar.com>
2025-09-14 10:33:20 +0300

    Adding k8s here and there.

---
+++ zsh/plugins/k8s.zsh
@@ -0,0 +1,25 @@
+k-cluster() {
+    local kubeconfig_path
+    kubeconfig_path=$(tv k8s-cluster)
+
+    if [[ $? -eq 0 && -n "$kubeconfig_path" ]]; then
+        export KUBECONFIG="$kubeconfig_path"
+        echo "KUBECONFIG set to: $KUBECONFIG"
+    else
+        echo "Error: Failed to get kubeconfig path from 'tv k8s-cluster'" >&2
+        return 1
+    fi
+}
+
+k-ns() {
+    local kube_ns
+    kube_ns=$(tv k8s-namespace)
+
+    if [[ $? -eq 0 && -n "$kube_ns" ]]; then
+        export KUBE_NS="$kube_ns"
+        echo "KUBE_NS set to: $KUBE_NS"
+    else
+        echo "Error: Failed to get namespace from 'tv k8s-namespace'" >&2
+        return 1
+    fi
+}
