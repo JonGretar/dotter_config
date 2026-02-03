e368961f7ac3bda1508f71296030dc1ad66da868
Jón Grétar Borgþórsson <jon@montel.fi>
2026-02-03 20:22:30 +0200

    yazy: Plugin updates

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,2 +1,20 @@
 -- DuckDB plugin configuration
 require("starship"):setup()
+
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = true, mode="jump" },
+	persist = "vim",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	custom_desc_input = false,
+	show_keys = false,
+	notify = {
+		enable = false,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
