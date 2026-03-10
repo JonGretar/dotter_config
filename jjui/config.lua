e4569b9acb4fb931f55d93346e7c2e74d909f7e7
Jón Grétar Borgþórsson <jon@montel.fi>
2026-03-10 12:02:10 +0200

    jjui: Upgrade to 0.10.x

---
+++ jjui/config.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable: undefined-global -- jjui injects: context, flash, exec_shell
+
+function setup(config)
+  -- register actions and bindings here
+
+    config.action("show diff in diffnav", function()
+      local change_id = context.change_id()
+      if not change_id or change_id == "" then
+        flash({ text = "No revision selected", error = true })
+        return
+      end
+
+      exec_shell(string.format("jj diff -r %q --git --color never | zed -", change_id))
+    end, { desc = "show diff in diffnav", key = "ctrl+d", scope = "revisions" })
+
+end
